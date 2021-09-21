84abb8d33efda8fdcac0280e1052587f71f55626
MGreco2112 <mgreco2112@tutanota.com>
2021-09-21 11:50:03 -0400

    finished challenge

---
+++ fccaddressbook.js
@@ -0,0 +1,66 @@
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+    let returnPhrase;
+
+    for (let i = 0; i < contacts.length; i++) {
+        console.log(i);
+        if (contacts[i].firstName === name && contacts[i].hasOwnProperty(prop)) {
+            return contacts[i][prop];
+        } else if (contacts[i].firstName === name && !contacts[i].hasOwnProperty(prop)) {
+            return "No such property";
+        } else if (contacts[i].firstName !== name) {
+            returnPhrase = "No such contact";
+        }
+    }
+
+    return returnPhrase;
+  // Only change code above this line
+}
+
+console.log(lookUpProfile("Akira", "address"));
+
+/*
+
+lookUpProfile("Kristian", "lastName") should return the string Vos
+
+lookUpProfile("Sherlock", "likes") should return ["Intriguing Cases", "Violin"]
+
+lookUpProfile("Harry", "likes") should return an array
+
+Passed
+lookUpProfile("Bob", "number") should return the string No such contact
+
+Passed
+lookUpProfile("Bob", "potato") should return the string No such contact
+
+lookUpProfile("Akira", "address") should return the string No such property
+*/
